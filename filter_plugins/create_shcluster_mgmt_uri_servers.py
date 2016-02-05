d0649071663534302370ea52ef8f7952e5785277
Mika Borner <mika.borner@gmail.com>
2016-02-05 09:06:39 +0100

    Fixes #9

--- filter_plugins/create_shcluster_mgmt_uri_servers.py
+++ filter_plugins/create_shcluster_mgmt_uri_servers.py
@@ -3,10 +3,12 @@ def create_shcluster_mgmt_uri_servers (serverlist):
   servers_normalized = []
 
   for item in serverlist:
-   if type(item) is str:
-     item=[item]
-   for sublist in item:
-     servers_normalized.append(sublist)
+    if type(item) is unicode:
+     item=str(item)
+    if type(item) is str:
+      item=[item]
+    for sublist in item:
+      servers_normalized.append(sublist)
 
   servers = []
   [servers.append(item) for item in servers_normalized if item not in servers]
